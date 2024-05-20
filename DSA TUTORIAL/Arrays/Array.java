accea1f910909986b7773e8317125759c572909b
Jay Vegad <jayvegad10@gmail.com>
2024-05-20 10:18:37 +0530

    Sum Programme

--- DSA TUTORIAL/Arrays/Array.java
+++ DSA TUTORIAL/Arrays/Array.java
@@ -1,5 +1,7 @@
 public class Array {
     public static void main(String[] args) {
-        System.err.println("Hello World");
+        int a = 40;
+        int b = 40;
+        System.out.println(a+b);
     }
 }
