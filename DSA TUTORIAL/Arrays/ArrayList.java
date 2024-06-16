8bf4c553dd08053980e094033b951dbcb1f3751f
Jay Vegad <143251488+Vegadjay@users.noreply.github.com>
2024-06-16 23:54:41 +0530

    Delete DSA TUTORIAL/Arrays/ArrayList.java

--- DSA TUTORIAL/Arrays/ArrayList.java
+++
@@ -1,7 +0,0 @@
-public class ArrayList {
-    public static void main(String[] args) {
-        // Write your code here
-
-                                                                                                                                                                                    
-    }
-}
